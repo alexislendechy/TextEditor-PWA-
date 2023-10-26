4490ce80c5f942fca7439ee26f66522841c62e33
alexislendechy <135558007+alexislendechy@users.noreply.github.com>
2023-10-26 11:16:43 -0600

    code + readme

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,40 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default behavior
+  event.preventDefault();
+  // Store the event for later use
+  window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  // TODO: Update UI to indicate that the app can be installed
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  const deferredPrompt = window.deferredPrompt;
+
+  if (deferredPrompt) {
+    // Show the installation prompt to the user
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    const choiceResult = await deferredPrompt.userChoice;
+
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+
+    // Reset the deferredPrompt variable
+    window.deferredPrompt = null;
+  }
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed by user');
+  // TODO: Update UI to indicate that the app has been installed
+});
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,27 +3,73 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
 
-module.exports = () => {
   return {
-    mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install'],
+      }),
+      new WebpackPwaManifest({
+        name: 'Your Text Editor',
+        short_name: 'Text Editor',
+        description: 'Just Another Text Editor',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'images/[name].[ext]',
+              },
+            },
+          ],
+        },
       ],
     },
   };
