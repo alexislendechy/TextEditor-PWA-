68b34650575381aeb7cda2130826c932d46999a8
alexislendechy <135558007+alexislendechy@users.noreply.github.com>
2023-11-04 14:50:41 -0600

    json file in server was misssing among other modifications

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -25,13 +25,15 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('jate', 'readonly');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
   result
   ? console.log('Data retrieved from the database', result.value)
   : console.log('Data not found in the database');
+  return result?.value
 };
 
 initdb();
